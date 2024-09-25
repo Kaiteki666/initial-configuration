bdea0407e7ff8c718506045a52657c21a5afbcde
Kaiteki666 <157815575+Kaiteki666@users.noreply.github.com>
2024-09-25 17:22:46 +0300

    Update install-node-red.sh

--- install-node-red.sh
+++ install-node-red.sh
@@ -1,40 +1,16 @@
 #!/bin/bash
 
-# 1. Запускаємо скрипт для встановлення Node-RED та Node.js
-bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-root --confirm-install --skip-pi --restart --no-init
+# 1. Виконуємо встановлення Node-RED
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-root --confirm-install --skip-pi  --restart --no-init
 
-# 2. Генеруємо хеш пароля
+# 2. Генеруємо парольний хеш
 PASSWORD="23142314qW"
-HASHED_PASSWORD=$(node-red admin hash-pw <<EOF
-$PASSWORD
-EOF
-)
+HASHED_PASSWORD=$(echo "$PASSWORD" | node-red admin hash-pw)
 
-# 3. Перевіряємо чи згенеровано хеш
-if [ -z "$HASHED_PASSWORD" ]; then
-  echo "Хеш пароля не згенеровано!"
-  exit 1
-fi
-
-# 4. Відкриваємо settings.js та вносимо зміни
+# 3. Оновлюємо файл settings.js
 SETTINGS_FILE="$HOME/.node-red/settings.js"
-SEARCH_TEXT="/** To password protect the Node-RED editor and admin API, the following"
-NEW_CONFIG="adminAuth: {
-        \"type\": \"credentials\",
-        \"users\": [
-            {
-                \"username\": \"kaiteki\",
-                \"password\": \"$HASHED_PASSWORD\",
-                \"permissions\": \"*\"
-            }
-        ]
-},"
 
-# Використовуємо sed для пошуку та додавання конфігурації
-if grep -q "$SEARCH_TEXT" "$SETTINGS_FILE"; then
-  # Вставляємо нову конфігурацію після коментаря
-  sed -i "/$SEARCH_TEXT/a $NEW_CONFIG" "$SETTINGS_FILE"
-  echo "Файл settings.js оновлено!"
-else
-  echo "Не знайдено потрібного коментаря у файлі settings.js!"
-fi
+# Використовуємо sed для розкоментування та заміни потрібних рядків
+sed -i '/\/\/adminAuth: {/{N;N;N;N;s/\/\/adminAuth: {/adminAuth: {/;s/\/\/ *type: "credentials",/    type: "credentials",/;s/\/\/ *users: \[{/    users: \[{/;s/\/\/ *username: "admin",/        username: "kaiteki",/;s/\/\/ *password: "PASSWORD"/        password: "'"$HASHED_PASSWORD"'"/;s/\/\/ *permissions: "*"/        permissions: "*"/;s/\/\/ *}\]/    }]/;s/\/\/ *}\]}/    }]/}' "$SETTINGS_FILE"
+
+echo "Node-RED встановлено та налаштовано з користувачем kaiteki і паролем $PASSWORD."
