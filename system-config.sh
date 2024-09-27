a8c3a6b0e35827bb56a1c03dc433ecaff0c4ac2a
Kaiteki666 <157815575+Kaiteki666@users.noreply.github.com>
2024-09-27 12:24:05 +0300

    Update system-config.sh

--- system-config.sh
+++ system-config.sh
@@ -1,1 +1,41 @@
+#!/bin/bash
+
+# Перевірка наявності всіх аргументів
+if [ $# -ne 5 ]; then
+  echo "Usage: $0 <login> <change_password> <cloudflare_token> <nodered_username> <nodered_password>"
+  exit 1
+fi
+
+# Зчитування аргументів
+LOGIN=$1
+CHANGE_PASSWORD=$2
+CLOUDFLARE_TOKEN=$3
+NODERED_USERNAME=$4
+NODERED_PASSWORD=$5
+
+# Оновлення пакетів
+echo "Updating packages..."
+sudo apt update
+sudo apt upgrade -y
+
+# Налаштування watchdog таймера
+echo "Configuring watchdog timer..."
+curl -sL https://raw.githubusercontent.com/Kaiteki666/initial-configuration/refs/heads/main/configurate-watchdog.sh | sudo bash -s
+
+# Зміна користувача
+echo "Changing user..."
+curl -sL https://raw.githubusercontent.com/Kaiteki666/initial-configuration/refs/heads/main/change_user.sh | sudo bash -s -- "$LOGIN" "$CHANGE_PASSWORD"
+
+# Встановлення cloudflared
+echo "Installing cloudflared..."
+curl -sL https://raw.githubusercontent.com/Kaiteki666/initial-configuration/main/install-cloudflared.sh | bash -s -- "$CLOUDFLARE_TOKEN"
+
+# Встановлення Node-RED
+echo "Installing Node-RED..."
+curl -sL https://raw.githubusercontent.com/Kaiteki666/initial-configuration/refs/heads/main/install-node-red.sh | sudo bash -s -- "$NODERED_USERNAME" "$NODERED_PASSWORD"
+
+# Перезавантаження системи
+echo "The system will reboot in 20 seconds..."
+sleep 20
+sudo reboot
 
